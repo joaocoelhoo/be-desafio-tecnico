0b612911d6b5c4e883b5bbe7ddca83993bf5526a
Joao Coelho <joaopaulomelocoelho@gmail.com>
2024-07-03 23:24:30 -0300

    Create filter input

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,10 +1,32 @@
-import React, { ReactNode } from 'react';
+import React, { ReactNode, useEffect, useState } from 'react';
 import Context from '../context/Context';
 import { EmployeeContextType, IEmployee } from '../@types/employees';
 import './Table.css';
 import { formatDate, formatPhoneNumber } from '../utils/formatFunctions';
 
 const Table: React.FC  = () => {
+  const { employees } = React.useContext(Context) as EmployeeContextType;
+  const [filteredEmployees, setFilteredEmployees] = useState<IEmployee[]>([]);
+
+  useEffect(() => {
+    setFilteredEmployees(employees)
+  }, [employees])
+
+  const searchEmployees = ({target}: React.ChangeEvent<HTMLInputElement>) => {
+    const inputValue = target.value.toLowerCase();
+
+    const filterEmployees = employees.filter((employee) => {
+      const name = employee.name.toLowerCase(); 
+      const job = employee.job.toLowerCase();
+      const phone = employee.phone;
+
+      const employeeFound = name.includes(inputValue) || job.includes(inputValue) || phone.includes(inputValue);
+
+      return employeeFound;
+    });
+
+    setFilteredEmployees(filterEmployees);
+  };
 
   const renderEmployees = (employees: IEmployee[]): ReactNode[] => {
     const employeesData = employees.map((employee, index) =>
@@ -35,27 +57,32 @@ const Table: React.FC  = () => {
   }
 
   return (
-    <Context.Consumer>
-      {(contextValue: EmployeeContextType | null) => (
-        <div>
-          <table>
-            <thead>
-              <tr>
-                <th>Foto</th>
-                <th>Nome</th>
-                <th>Cargo</th>
-                <th>Data de Admissão</th>
-                <th>Telefone</th>
-              </tr>
-            </thead>
-            <tbody>
-              { contextValue?.employees && renderEmployees(contextValue?.employees) }
-            </tbody>
-          </table>
-        </div>
-      )
-      }
-    </Context.Consumer>
+    <div>
+      <section>
+        <h4 className='title'>Funcionários</h4>
+      </section>
+      <section>
+        <input
+          type='text' 
+          id='search'
+          placeholder='Pesquisar'
+          onChange={ (event) => {searchEmployees(event)} } />
+      </section>
+      <table>
+        <thead className='columns'>
+          <tr>
+            <th>Foto</th>
+            <th>Nome</th>
+            <th>Cargo</th>
+            <th>Data de Admissão</th>
+            <th>Telefone</th>
+          </tr>
+        </thead>
+        <tbody>
+          { filteredEmployees && renderEmployees(filteredEmployees) }
+        </tbody>
+      </table>
+    </div>
   )
 }
 
